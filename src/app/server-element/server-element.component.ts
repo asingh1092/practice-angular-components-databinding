5049b60190b2eadc9ded221e09474982b3cc3a8d
Amrit Singh <asingh1092@gmail.com>
2021-07-15 21:40:54 -0400

    ContentChild decorator

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,6 +4,7 @@ import {
   AfterViewChecked,
   AfterViewInit,
   Component,
+  ContentChild,
   DoCheck,
   ElementRef,
   Input,
@@ -12,7 +13,7 @@ import {
   OnInit,
   SimpleChanges,
   ViewChild,
-  ViewEncapsulation
+  ViewEncapsulation,
 } from "@angular/core";
 
 @Component({
@@ -35,6 +36,7 @@ export class ServerElementComponent
   @Input("srvElement") element: { type: string; name: string; content: string };
   @Input() name: string;
   @ViewChild("heading", { static: true }) header: ElementRef;
+  @ContentChild("contentParagraph", { static: true }) paragraph: ElementRef;
 
   constructor() {
     console.log("constructor() called!");
@@ -48,6 +50,9 @@ export class ServerElementComponent
   ngOnInit(): void {
     console.log("ngOnInit() called!");
     console.log("Text Content: " + this.header.nativeElement.textContent);
+    console.log(
+      "Text Content of paragraph: " + this.paragraph.nativeElement.textContent
+    );
   }
 
   ngDoCheck(): void {
@@ -56,15 +61,17 @@ export class ServerElementComponent
 
   ngAfterContentInit(): void {
     console.log("ngAfterContentInit() called!");
+    console.log(
+      "Text Content of paragraph: " + this.paragraph.nativeElement.textContent
+    );
   }
 
   ngAfterContentChecked(): void {
     console.log("ngAfterContentChecked() called!");
-    console.log("Text Content: " + this.header.nativeElement.textContent);
-
   }
   ngAfterViewInit(): void {
     console.log("ngAfterViewInit() called!");
+    console.log("Text Content: " + this.header.nativeElement.textContent);
   }
   ngAfterViewChecked(): void {
     console.log("ngAfterViewChecked() called!");
